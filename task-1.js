fc994404aedcd1f1f72288fa5357d1cd73f6f48b
VitaliiHryshchenko <130399794+VitaliiHryshchenko@users.noreply.github.com>
2024-09-15 14:39:05 +0000

    Deploying to gh-pages from @ VitaliiHryshchenko/Practice_blanded@12a6d03a0dae74cd10c846e81de17c0ed51b085d 🚀

--- task-1.js
+++ task-1.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const s=t=>{t.preventDefault();const e=t.target.elements.taskName.value.trim();console.log(e)},a={taskForm:document.querySelector("#task-form"),taskList:document.querySelector("#task-list"),taskName:document.querySelector('input[name="taskName"]')};a.taskForm.addEventListener("submit",s);
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const e=t=>{t.preventDefault(),t.target.elements.taskName.value.trim()},a={taskForm:document.querySelector("#task-form"),taskList:document.querySelector("#task-list"),taskName:document.querySelector('input[name="taskName"]')};a.taskForm.addEventListener("submit",e);
 //# sourceMappingURL=task-1.js.map
